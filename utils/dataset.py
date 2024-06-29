214cd4079a291e8f395d37791985ba18fd94bd6c
Shohruh Rakhmatov <122860237+Shohruh72@users.noreply.github.com>
2024-06-29 12:31:33 +0900

    Update dataset.py

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,58 +1,51 @@
 import os
-import cv2
 import glob
 import numpy as np
-from PIL import Image
+from skimage import io
 from torch.utils import data
-from utils.util import get_transforms
-from torchvision.transforms import functional as F
 
 
 class Dataset(data.Dataset):
-    def __init__(self, data_dir, is_train=True):
-        self.data_dir = data_dir
-        self.transform = get_transforms(is_train)
+    def __init__(self, data_dir, transform=None):
+        self.transform = transform
         self.images, self.labels = self.load_input(data_dir)
 
-    def __getitem__(self, index):
-        image = Image.open(self.images[index]).convert('RGB')
-        label = Image.open(self.labels[index]).convert('L')
+    def __getitem__(self, idx):
+        image = io.imread(self.images[idx])
 
-        image, label = self.process(image, label)
+        if 0 == len(self.labels):
+            label_3 = np.zeros(image.shape)
+        else:
+            label_3 = io.imread(self.labels[idx])
+
+        label = np.zeros(label_3.shape[0:2])
+        if 3 == len(label_3.shape):
+            label = label_3[:, :, 0]
+        elif 2 == len(label_3.shape):
+            label = label_3
+
+        if 3 == len(image.shape) and 2 == len(label.shape):
+            label = label[:, :, np.newaxis]
+        elif 2 == len(image.shape) and 2 == len(label.shape):
+            image = image[:, :, np.newaxis]
+            label = label[:, :, np.newaxis]
+
+        image = np.ascontiguousarray(image)
+        label = np.ascontiguousarray(label)
+
+        sample = {'image': image, 'label': label}
 
         if self.transform:
-            image = self.transform(image)
-            label = F.resize(label, size=image.shape[-2:])
-            label = F.to_tensor(label)
+            sample = self.transform(sample)
 
-        return {'image': image, 'label': label}
+        return sample
 
     def __len__(self):
         return len(self.images)
 
     @staticmethod
     def load_input(data_dir):
-        images = glob.glob(os.path.join(data_dir, 'images', '*' + '.jpg'))
-        labels = [os.path.join(data_dir, 'labels', os.path.basename(f).replace('.jpg', '.png')) for f in images]
+        images = glob.glob(os.path.join(data_dir, '*.jpeg'))
+        # labels = [os.path.join(data_dir, 'labels', os.path.basename(f).replace('.jpg', '.png')) for f in images]
 
-        return images, labels
-
-    @staticmethod
-    def process(image, label):
-        # Convert PIL Images to numpy arrays
-        image_np = np.array(image)
-        label_np = np.array(label)
-
-        # Ensure label is 2D
-        if len(label_np.shape) == 3:
-            label_np = label_np[:, :, 0]
-
-        # Ensure image is 3D
-        if len(image_np.shape) == 2:
-            image_np = np.expand_dims(image_np, axis=2)
-
-        # Convert back to PIL Images
-        image = Image.fromarray(image_np)
-        label = Image.fromarray(label_np)
-
-        return image, label
+        return images, []
